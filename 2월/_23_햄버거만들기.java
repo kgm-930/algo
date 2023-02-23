ce023379d54a11af2346a1b2f975d18f48b61039
kimmin930 <kimmin930@naver.com>
2023-02-24 01:00:00 +0900

    230223

--- 2월/_23_햄버거만들기.java
+++ 2월/_23_햄버거만들기.java
@@ -1,6 +1,30 @@
+import java.util.Stack;
+
 class Solution {
     public int solution(int[] ingredient) {
         int answer = 0;
+        Stack<Integer> stack = new Stack<>();
+
+        for (int ing : ingredient) {
+            stack.add(ing);
+
+            if (stack.size() >= 4) {
+                int b1 = stack.pop();
+                int m = stack.pop();
+                int v = stack.pop();
+                int b2 = stack.pop();
+
+                if ((b1 == 1) && (m == 3) && (v == 2) && (b2 == 1)) {
+                    answer++;
+                } else {
+                    stack.add(b2);
+                    stack.add(v);
+                    stack.add(m);
+                    stack.add(b1);
+                }
+            }
+        }
+
         return answer;
     }
 }
