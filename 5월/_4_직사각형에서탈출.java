8158c2a88495d61e0dadd4bab9ec0560c67c5bb1
kimmin930 <kimmin930@naver.com>
2023-05-04 22:09:41 +0900

    230504

---
+++ 5월/_4_직사각형에서탈출.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class _4_직사각형에서탈출 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+        int b = Integer.parseInt(st.nextToken());
+        int c = Integer.parseInt(st.nextToken());
+        int d = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.min(Integer.min(a, b), Integer.min(c - a, d - b)));
+    }
+}
